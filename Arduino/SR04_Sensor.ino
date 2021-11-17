47ab23cbf06bd6e85c2deafef11cdd8c2d8d44b7
Rich Cochrane <rich.cochrane@bigi.org.uk>
2021-11-17 14:25:49 +0000

    Create SR04_Sensor.ino

---
+++ Arduino/SR04_Sensor.ino
@@ -0,0 +1,76 @@
+/*
+  :Based on an example by Dragos Calin
+*/
+ 
+ 
+/*define the sensor's pins*/
+uint8_t trigPin = 3;
+uint8_t echoPin = 2;
+ 
+unsigned long timerStart = 0;
+int TIMER_TRIGGER_HIGH = 10;
+int TIMER_LOW_HIGH = 2;
+ 
+float timeDuration, distance;
+ 
+/*The states of an ultrasonic sensor*/
+enum SensorStates {
+  TRIG_LOW,
+  TRIG_HIGH,
+  ECHO_HIGH
+};
+ 
+SensorStates _sensorState = TRIG_LOW;
+ 
+void startTimer() {
+  timerStart = millis();
+}
+ 
+bool isTimerReady(int mSec) {
+  return (millis() - timerStart) < mSec;
+}
+ 
+/*Sets the data rate in bits per second and configures the pins */
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+ 
+  /*Switch between the ultrasonic sensor states*/
+  switch (_sensorState) {
+    /* Start with LOW pulse to ensure a clean HIGH pulse*/
+    case TRIG_LOW: {
+        digitalWrite(trigPin, LOW);
+        startTimer();
+        if (isTimerReady(TIMER_LOW_HIGH)) {
+          _sensorState = TRIG_HIGH;
+        }
+      } break;
+      
+    /*Triggered a HIGH pulse of 10 microseconds*/
+    case TRIG_HIGH: {
+        digitalWrite(trigPin, HIGH);
+        startTimer();
+        if (isTimerReady(TIMER_TRIGGER_HIGH)) {
+          _sensorState = ECHO_HIGH;
+        }
+      } break;
+ 
+    /*Measures the time that ping took to return to the receiver.*/
+    case ECHO_HIGH: {
+        digitalWrite(trigPin, LOW);
+        timeDuration = pulseIn(echoPin, HIGH);
+        /*
+           distance = time * speed of sound
+           speed of sound is 340 m/s => 0.034 cm/us
+        */
+        Serial.println(timeDuration * 0.034 / 2);
+        _sensorState = TRIG_LOW;
+      } break;
+      
+  }//end switch
+  
+}//end loop
