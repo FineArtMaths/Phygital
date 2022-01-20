b0a0479ec37cb79e35357892de327df8818c7788
Rich Cochrane <rich.cochrane@bigi.org.uk>
2022-01-20 08:46:20 +0000

    Support multiple search terms

--- FlickrAPIScraper/scraper.py
+++ FlickrAPIScraper/scraper.py
@@ -3,40 +3,49 @@ import requests
 
 # To apply for a Flickr API key, log into Flickr and see here:
 # https://www.flickr.com/services/apps/create/apply
-FLICKR_PUBLIC = "REDACTED"
-FLICKR_SECRET = "REDACTED"
+FLICKR_PUBLIC = "250d9d41cbb48b470e0e4407692b3692"
+FLICKR_SECRET = "d9e680ba0fcb158c"
 
-# Fill in the search term and number of results here.
-search_term = "hippo"
-num_results = 20
+# Fill in the search terms here.
+# Try https://www.indifferentlanguages.com
+search_terms = ["hippopotamus", "Nilpferd"]
 
 # Explanation of the "extras" tags:
 # https://librdf.org/flickcurl/api/flickcurl-searching-search-extras.html
 tag = "url_m"
+num_results = 500 # The maximum Flickr will provide
 
-# Go to Flickr and request the URLs of the images
-flickr = fapi.FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format="parsed-json")
-cats = flickr.photos.search(
-  text=search_term,
-  per_page=num_results,
-  extras=tag
-)
+for search_term in search_terms:
+  print("Downloading images for:", search_term)
+  
+  # Go to Flickr and request the URLs of the images
+  flickr = fapi.FlickrAPI(FLICKR_PUBLIC, FLICKR_SECRET, format="parsed-json")
+  cats = flickr.photos.search(
+    text=search_term,
+    per_page=num_results,
+    extras=tag
+  )
 
-# Download each image into its own file.
-# If requesting a large number of images, it would be
-# wise to "throttle" this part of the process to avoid
-# gettting an IP ban from Flickr. 
-count = 0
-for p in cats["photos"]["photo"]:
-  url = p[tag]
-  f = open("images/image_" + str(count) + ".jpg", "wb")
-  response = requests.get(url, stream=True)
-  if not response.ok:
-      print(response)
-  else:
-    for block in response.iter_content(1024):
-      if not block:
-        break
-      f.write(block)
-  f.close()
-  count += 1
+  # Download each image into its own file.
+  count = 0
+  for p in cats["photos"]["photo"]:
+    if tag in p:
+        url = p[tag]
+        response = requests.get(url, stream=True)
+        if not response.ok:
+            print(response)
+        else:
+          try:
+            f = open("images/" + search_term + "_image_" + str(count) + ".jpg", "wb")
+            for block in response.iter_content(1024):
+              if not block:
+                break
+              f.write(block)
+          except:
+            pass
+          finally:
+            f.close()
+        count += 1
+        if count % 100 == 0:
+            print("    ", count, "of", num_results)
+
